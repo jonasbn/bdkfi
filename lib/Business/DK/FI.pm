58fb2951987876a86015740481365576a805043f
jonasbn <jonasbn@logiclab.dk>
2008-09-06 19:37:31 +0000

    Initial revision, of last snapshop migrated from CVS (without history)
    git-svn-id: file:///Users/jonasbn/Desktop/svnrepos/local-svn/BDKFI/trunk@5617 e6edf6fb-f266-4316-afb4-e53d95876a76

---
+++ lib/Business/DK/FI.pm
@@ -0,0 +1,57 @@
+package Business::DK::FI;
+
+# $Id$
+
+use strict;
+use vars qw($VERSION @ISA);
+
+$VERSION = '0.01';
+@ISA = ();
+
+1;
+
+__END__
+
+=head1 NAME
+
+Business::DK::FI -
+
+=head1 SYNOPSIS
+
+=head1 ABSTRACT
+
+=head1 DESCRIPTION
+
+=head1 BUGS
+
+Please report issues via CPAN RT:
+
+  http://rt.cpan.org/NoAuth/Bugs.html?Dist=Business-DK-FI
+
+or by sending mail to
+
+  bug-Business-DK-FI@rt.cpan.org
+
+=head1 SEE ALSO
+
+=over
+
+=item 
+
+=back
+
+=head1 AUTHOR
+
+Jonas B. Nielsen, (jonasbn) - C<< <jonasbn@cpan.org> >>
+
+=head1 COPYRIGHT
+
+Business-DK-FI is (C) by Jonas B. Nielsen, (jonasbn) 2006
+
+Business-DK-FI is released under the artistic license
+
+The distribution is licensed under the Artistic License, as specified
+by the Artistic file in the standard perl distribution
+(http://www.perl.com/language/misc/Artistic.html).
+
+=cut
