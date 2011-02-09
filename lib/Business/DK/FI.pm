fc3366f93b8346d0ad34a7d41a5ec5edc09c642c
jonasbn <jonasbn@logiclab.dk>
2011-02-09 06:03:55 +0000

    BDKFI-3 #time 1h #comment Initiated implementation
    git-svn-id: file:///Users/jonasbn/Desktop/svnrepos/local-svn/BDKFI/trunk@7441 e6edf6fb-f266-4316-afb4-e53d95876a76

--- lib/Business/DK/FI.pm
+++ lib/Business/DK/FI.pm
@@ -3,10 +3,43 @@ package Business::DK::FI;
 # $Id$
 
 use strict;
-use vars qw($VERSION @ISA);
+use warnings;
+use vars qw($VERSION @EXPORT_OK @ISA);
+use Params::Validate qw(validate_pos SCALAR);
+use Readonly;
+use Business::DK::CVR qw(_calculate_sum);
 
 $VERSION = '0.01';
-@ISA = ();
+@EXPORT_OK = qw(validate validateFI generate);
+@ISA = qw(Exporter);
+
+use constant MODULUS_OPERAND => 10;
+use constant INVALID         => 0;
+use constant VALID           => 1;
+
+Readonly my @controlcifers => qw(1 2 1 2 1 2 1 2 1 2 1 2 1 2);
+
+sub validateFI {
+    return validate(shift);
+}
+
+sub validate {
+    my ($fi_number) = @_;
+
+    validate_pos( @_, { type => SCALAR, regex => qr/^\d{16}$/ } );
+
+    my $sum = _calculate_sum( $fi_number, \@controlcifers );
+
+    if ( $sum % MODULUS_OPERAND ) {
+        return INVALID;
+    } else {
+        return VALID;
+    }
+}
+
+sub generate {
+    
+}
 
 1;
 
@@ -14,7 +47,7 @@ __END__
 
 =head1 NAME
 
-Business::DK::FI -
+Business::DK::FI - validation of Danish FI numbers
 
 =head1 SYNOPSIS
 
